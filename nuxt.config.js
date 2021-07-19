c149e63fe1844b94f3d17a700c9991f5b0ce8812
AlisonV2 <alison.vandromme007@gmail.com>
2021-07-19 20:01:47 +0200

    changed titleTemplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - portfolio-front",
+    titleTemplate: "%s - Portfolio",
     title: "Alison Vandromme",
     meta: [
       { charset: "utf-8" },
