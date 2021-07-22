82cfa21e7d19a4fe2a7c5b911b0d9fa078f7860d
AlisonV2 <alison.vandromme007@gmail.com>
2021-07-22 18:46:04 +0200

    created contact page

---
+++ components/utils/AppForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form ref="form">
+    <div class="input-block">
+      <input type="text" required />
+      <span class="placeholder">{{ $t("contact.form.name") }}</span>
+    </div>
+    <div class="input-block">
+      <input type="email" required />
+      <span class="placeholder">{{ $t("contact.form.email") }}</span>
+    </div>
+    <div class="input-block">
+      <textarea required />
+      <span class="placeholder">{{ $t("contact.form.msg") }}</span>
+    </div>
+    <div class="form-btn">
+      <AppButton type="submit" @click.prevent="onSubmit">{{
+        $t("contact.btn")
+      }}</AppButton>
+    </div>
+  </form>
+</template>
+
+<style lang="scss">
+.form-btn {
+  padding: 2rem 0rem;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div class="col-12 col-md-5 contact-text">
+      <h2>{{ $t("contact.title") }}</h2>
+      <p class="description">
+        {{ $t("contact.description") }}
+      </p>
+      <h4>contact@alisonvandromme.com</h4>
+    </div>
+    <div class="col-12 col-md-7 contact-form">
+      <AppForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppForm from "@/components/utils/AppForm";
+
+export default {
+  components: {
+    AppForm
+  }
+};
+</script>
+
+<style lang="scss">
+.contact-form {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
